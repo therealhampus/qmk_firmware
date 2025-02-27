2189a9f92f6ee144302389b741c466d0c8b56aec
therealhampus <hampus.eriksson@proton.me>
2025-02-27 19:46:08 +0100

    Lily58 with Home Row Mods
    Uses accordion to make HRM nicer to use

---
+++ keyboards/lily58/keymaps/supertuba/config.h
@@ -0,0 +1,45 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+
+#define TAPPING_TERM 150
+#define QUICK_TAP_TERM 0
+#define QUICK_TAP_TERM_PER_KEY
+#define PERMISSIVE_HOLD
+
+#undef RGBLED_NUM
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 27
+#define RGBLIGHT_LIMIT_VAL 120
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 17
+#define RGBLIGHT_VAL_STEP 17
+
+// achordion
+#define ACHORDION_STREAK
+
+// Caps word enabled
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
