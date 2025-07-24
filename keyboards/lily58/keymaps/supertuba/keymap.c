bce245d7aa31f823b828a77c58a36496505cfb95
therealhampus <hampus.eriksson@proton.me>
2025-07-24 08:56:30 +0200

    feat: Replace standard CTRL with Escape
    Home row mods means standard CTRL button can be re-purposed. Escape is
    used for emacs-meow to exit insert mode.

--- keyboards/lily58/keymaps/supertuba/keymap.c
+++ keyboards/lily58/keymaps/supertuba/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * | ESC  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_QWERTY] = LAYOUT(
     QK_GESC, KC_1,         KC_2,         KC_3,         KC_4,         KC_5,                        KC_6,   KC_7,         KC_8,         KC_9,         KC_0,            KC_EQL,
     KC_TAB,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                        KC_Y,   KC_U,         KC_I,         KC_O,         KC_P,            KC_MINS,
-    KC_LCTL, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,                        KC_H,   RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
+    QK_GESC, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,                        KC_H,   RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
     KC_LSFT, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B, KC_LBRC,      KC_RBRC, KC_N,   KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_RSFT,
                            KC_LALT,      KC_LGUI,      MO(1),        KC_SPC,                      KC_ENT, MO(2),        KC_BSPC,      KC_RGUI
   ),
