377abbab15a77b9e8f66b9075b9f0617f216b8c1
therealhampus <hampus.eriksson@proton.me>
2026-09-18 10:01:02 +0200

    fix: remove deprecated config item

--- keyboards/lily58/keymaps/supertuba/config.h
+++ keyboards/lily58/keymaps/supertuba/config.h
@@ -31,7 +31,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define CHORDAL_HOLD
 
 #undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 27
 #define RGBLIGHT_LIMIT_VAL 120
 #define RGBLIGHT_HUE_STEP 10
